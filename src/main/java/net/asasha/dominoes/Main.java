d86e1a2aed0449885900403e453b5eaa4724d93f
Alex <sumy.ua@gmail.com>
2017-04-07 16:31:56 +0300

    Start of project - 7.04.2017

---
+++ src/main/java/net/asasha/dominoes/Main.java
@@ -0,0 +1,13 @@
+package net.asasha.dominoes;
+
+/**
+ * Start of project - 7.04.2017
+ *
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Dominoes!");
+    }
+
+}
